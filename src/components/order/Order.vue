3b46eade303a3f7a8a2cabb976731b1551d759ee
Mr-Zzyf1 <1070678414@qq.com>
2021-04-26 18:41:15 +0800

    'edit-order.vue'

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -10,8 +10,8 @@
         <el-card>
             <el-row>
                 <el-col :span="8">
-                    <el-input placeholder="请输入内容"  class="input-with-select">
-                        <el-button slot="append" icon="el-icon-search"></el-button>
+                    <el-input placeholder="请输入内容"  class="input-with-select" v-model="queryInfo.query" clearable @clear="getOrderList">
+                        <el-button slot="append" icon="el-icon-search" @click="getOrderList"></el-button>
                     </el-input>
                 </el-col>
             </el-row>
@@ -34,11 +34,11 @@
                     </template>
                 </el-table-column>
                 <el-table-column label="操作" width="330px">
-                    <template >
+                    <template slot-scope="scope">
                         <el-button type="primary" size="mini" icon="el-icon-edit" @click="showBox"></el-button>
                         <el-button type="success" size="mini" icon="el-icon-location" @click="showProgressBox"></el-button>
-                        <el-button type="primary" size="mini">订单详情</el-button>
-                        <el-button type="warning" size="mini">修改订单状态</el-button>
+                        <el-button type="primary" size="mini" @click="orderMessage(scope.row)">订单详情</el-button>
+                        <el-button type="warning" size="mini" @click="modifyOrder(scope.row.order_id)">修改订单状态</el-button>
                     </template>
                 </el-table-column>
             </el-table>
@@ -103,6 +103,82 @@
                 <el-button type="primary" @click="progressVisible = false">确 定</el-button>
             </span>
         </el-dialog>
+
+        <!-- 订单详情 -->
+        <el-dialog
+            title="订单详情"
+            :visible.sync="orderMessageVisible"
+            width="50%">
+            <!-- 详情信息 -->
+            <el-form ref="orderMessageRef" :model="orderMessageform" label-width="80px">
+                <el-form-item label="订单ID:">
+                    <el-input v-model="orderMessageform.order_id" disabled></el-input>
+                </el-form-item>
+                <el-form-item label="订单编号:">
+                    <el-input v-model="orderMessageform.order_number" disabled></el-input>
+                </el-form-item>
+                <el-form-item label="订单价格:">
+                    <el-input v-model="orderMessageform.order_price" disabled></el-input>
+                </el-form-item>
+                <el-form-item label="是否支付:">
+                    <!-- <el-input v-model="orderMessageform.order_pay" disabled></el-input> -->
+                    <p>{{this.orderMessageform.order_pay=='0'? '未支付' :'已支付'}}</p>
+
+                </el-form-item>
+                <el-form-item label="是否发货:">
+                    <el-input v-model="orderMessageform.is_send" disabled></el-input>
+                </el-form-item>
+                <el-form-item label="下单时间:">
+                    <!-- <el-input v-model="orderMessageform.create_time " disabled></el-input> -->
+                    <p>{{this.orderMessageform.create_time | dateFormat}}</p>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button type="primary" @click="orderMessageVisible = false">关 闭</el-button>
+            </span>
+        </el-dialog>
+
+        <!-- 修改订单对话框 -->
+        <el-dialog
+          title="修改订单状态"
+          :visible.sync="modifyOrderVisible"
+          width="50%"
+          @close="ddDialogclosed"
+        >
+          <!-- 提示消息 -->
+            <el-alert :closable="false" title="是否发货：1:已经发货、0:未发货" type="info"> </el-alert>
+            <el-alert :closable="false" title="支付方式：1 支付宝、2 微信、3 银行卡" type="info"> </el-alert>
+            <el-alert :closable="false" title="订单价格：只能为数字" type="info"> </el-alert>
+            <el-alert :closable="false" title="支付状态：1 已付款、0 未付款" type="info"> </el-alert>
+          <div class="el_formbox">
+            <el-form
+              :model="modifyOrderInfo"
+              ref="modifyorderRef"
+              :rules="modifyorderRules"
+              label-width="100px"
+            >
+              <el-form-item label="是否发货：" prop="is_send">
+                <el-input v-model="modifyOrderInfo.is_send" ></el-input>
+              </el-form-item>
+              <el-form-item label="支付方式：" prop="order_pay">
+                <el-input v-model="modifyOrderInfo.order_pay" ></el-input>
+              </el-form-item>
+              <el-form-item label="订单价格：" prop="order_price">
+                <el-input v-model="modifyOrderInfo.order_price" ></el-input>
+              </el-form-item>
+              <el-form-item label="支付状态：" prop="pay_status">
+                <el-input v-model="modifyOrderInfo.pay_status" ></el-input>
+              </el-form-item>
+            </el-form>
+          </div>
+          <span slot="footer" class="dialog-footer">
+            <el-button @click="modifyOrderVisible = false">取 消</el-button>
+            <el-button type="primary" @click="modifyOrderTrue">确 定</el-button>
+          </span>
+        </el-dialog>
+
+
+
 
     </div>
 </template>
@@ -207,6 +283,48 @@ export default {
                 "location": ""
                 }
             ],
+
+            // 订单详情显示与隐藏
+            orderMessageVisible:false,
+
+            // 订单详情信息显示
+            orderMessageform:{
+                order_id:'',
+                order_number:'',
+                order_price:'', 
+                order_pay:'',
+                is_send:'',
+                create_time:'',
+            },
+            //控制修改订单对话框显示和隐藏
+            modifyOrderVisible: false,
+
+            //保存修改订单对应id
+            order_id: {
+                order_id: null,
+            },
+            //修改订单详情验证规则
+            modifyorderRules:{
+                is_send: [
+                { required: true, message: "只能为数字", trigger: "blur" },
+                ],
+                    order_pay: [
+                { required: true, message: "只能为数字", trigger: "blur" },
+                ],
+                    order_price: [
+                { required: true, message: "只能为数字", trigger: "blur" },
+                ],
+                    pay_status: [
+                { required: true, message: "只能为数字", trigger: "blur" },
+                ],
+            },
+            //监听修改订单对话框的数据
+            modifyOrderInfo: {
+                is_send: "",
+                order_pay: "",
+                order_price: null,
+                pay_status: "",
+            },
         };
     },
     methods: {
@@ -238,6 +356,65 @@ export default {
             // })
             this.progressVisible=true
         },
+        // 点击查找
+        search(){
+            // this.axios.
+            console.log(this.queryInfo.query);
+
+        },
+
+        // 查看订单详情
+        orderMessage(scope){
+            this.orderMessageVisible=true
+            console.log(scope.order_id);
+            this.axios.get('orders/'+scope.order_id).then(res=>{
+                console.log(res.data.data);
+                this.orderMessageform=res.data.data
+            })
+        },
+
+        //显示对话框并保存对应ID
+        modifyOrder(order_id) {
+        // console.log(order_id);
+        //保存id值
+        this.order_id.order_id = order_id;
+        // console.log(this.order_id.order_id);
+        //显示对话框
+        this.modifyOrderVisible = true;
+        },
+
+        // 确认修改订单
+        modifyOrderTrue() {
+        this.$refs.modifyorderRef.validate((valid) => {
+            if (!valid) {
+            return;
+            } else {
+            this.modifyOrderVisible = false;
+            this.axios({
+                url: "orders/" + this.order_id.order_id,
+                method: "put",
+                data: {
+                is_send: this.modifyOrderInfo.is_send,
+                order_pay: this.modifyOrderInfo.order_pay,
+                order_price: this.modifyOrderInfo.order_price,
+                pay_status: this.modifyOrderInfo.pay_status,
+                },
+            }).then((res) => {
+                // console.log(res);
+                if (res.meta.status === 201) {
+                this.getOderList();
+                return this.$message.success(res.meta.msg);
+                } else {
+                return this.$message.error(res.meta.msg);
+                }
+            });
+            }
+        });
+        },
+        //监听修改订单对话框状态
+        ddDialogclosed() {
+        this.$refs.modifyorderRef.resetFields();
+    },
     },
     components: {
         
